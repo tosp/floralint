421953ae9c78f3b0d6f33666c1f500cbd1cd5dfa
Perimetro20 <a01205778@itesm.mx>
2016-11-13 01:30:30 -0600

    Hello world version

--- floralint.py
+++ floralint.py
@@ -1,1 +1,13 @@
-# Empty file for now
+import urllib.request
+from bs4 import BeautifulSoup
+
+raw = urllib.request.urlopen('https://tosp.io')
+html = raw.read()
+#print(html)
+soup = BeautifulSoup(html, "html.parser")
+images = soup.findAll('img')
+
+for image in images:
+	if 'alt' not in image:
+		print('Shitty code, no alt html value')
+	print(image.attrs)
