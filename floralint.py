ac824c2bf5468b2a32e5763611d784075c1ed364
Perimetro20 <a01205778@itesm.mx>
2016-11-13 01:37:59 -0600

    All the code is fully linted

--- floralint.py
+++ floralint.py
@@ -3,7 +3,6 @@ from bs4 import BeautifulSoup
 
 raw = urllib.request.urlopen('https://tosp.io')
 html = raw.read()
-#print(html)
 soup = BeautifulSoup(html, "html.parser")
 images = soup.findAll('img')
 
@@ -11,4 +10,3 @@ for image in images:
     if 'alt' not in image:
         print('Shitty code, no alt html value')
     print(image.attrs)
-    
